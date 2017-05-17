cf2adaa06adf528a4a046276fdf6617ee394f7b6
louiscocks <99043889@mydavinci.nl>
2017-05-17 21:03:24 +0200

    Update.Framework

--- view/clients/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "../common/header.php";
-?>
-	<h1>Clients</h1>
-	<p>to be implemented soon</p>
-	
-<?php
-	include "../common/footer.php";
-?>
--- view/home/index.php
+++ view/home/index.php
@@ -2,8 +2,6 @@
  <h1>Welkom bij het PHP-FrameWork.</h1>
  <p>Je bent nu in home.</p>
   
-  <a href="student">Students</a><br>
   <a href="birthday">Calendar</a><br>
-  <a href="patient">Hospital</a>
 
 </div>
